44f3e3d0ce789fba62cf2b92608a978d539558f3
Kageyama R <kageyama.ryoma@aibos2100.onmicrosoft.com>
2024-06-20 11:48:23 +0900

    change login-state

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -7,13 +7,44 @@ import {
 import { auth } from "@/src/lib/firebase/clientApp";
 
 export function onAuthStateChanged(cb) {
-  return () => {};
+  return _onAuthStateChanged(auth, cb);
 }
 
 export async function signInWithGoogle() {
-  return;
+  const provider = new GoogleAuthProvider();
+
+  try {
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    console.error("Error signing in with Google", error);
+  }
 }
 
 export async function signOut() {
-  return;
+  try {
+    return auth.signOut();
+  } catch (error) {
+    console.error("Error signing out with Google", error);
+  }
+}
+
+async function fetchWithFirebaseHeaders(request) {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const installations = getInstallations(app);
+  const headers = new Headers(request.headers);
+  const [authIdToken, installationToken] = await Promise.all([
+    getAuthIdToken(auth),
+    getToken(installations),
+  ]);
+  headers.append("Firebase-Instance-ID-Token", installationToken);
+  if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
+  const newRequest = new Request(request, { headers });
+  return await fetch(newRequest);
+}
+
+async function getAuthIdToken(auth) {
+  await auth.authStateReady();
+  if (!auth.currentUser) return;
+  return await getIdToken(auth.currentUser);
 }
